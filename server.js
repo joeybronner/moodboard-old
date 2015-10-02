5cce999118af6d3442a0a7029e25e2d219252061
Joey Bronner <joeybronner@gmail.com>
2015-10-02 18:11:41 +0200

    Adding MongoDB (and dependencies in bower.json file)

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 /* Includes */
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+/*
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});*/
 
 // Connection
 connect().use(serveStatic(__dirname)).listen(8081);
